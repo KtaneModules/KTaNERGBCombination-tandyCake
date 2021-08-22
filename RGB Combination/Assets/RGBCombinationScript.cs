ba7e6641b7ac2695640710f27296784e6a38ebb4
Danny7007 <67610733+Danny7007@users.noreply.github.com>
2021-08-22 11:52:13 -0400

    Module Rework

--- RGB Combination/Assets/RGBCombinationScript.cs
+++ RGB Combination/Assets/RGBCombinationScript.cs
@@ -14,173 +14,142 @@ public class RGBCombinationScript : MonoBehaviour {
     public KMBombModule Module;
     public KMColorblindMode Colorblind;
 
-    public MeshRenderer[] LeftGrid;
-    public MeshRenderer[] RightGrid;
-    public KMSelectable[] Buttons;
-    public Material[] Colors;
-    public TextMesh[] leftCB;
-    public TextMesh[] rightCB;
-
+    public MeshRenderer[] leds;
+    public KMSelectable[] buttons;
+    public TextMesh[] cbTexts;
+    public MeshRenderer[] stageLEDs;
+    public Material unlit;
     static int moduleIdCounter = 1;
     int moduleId;
     private bool moduleSolved;
     public bool cbON;
+    private Color[] colors = { new Color32(0x31, 0x31, 0x31, 0xFF), Color.blue, Color.green, Color.cyan, Color.red, Color.magenta, Color.yellow, Color.white };
+    private string[] colorNames = { "Black", "Blue", "Green", "Cyan", "Red", "Magenta", "Yellow", "White" };
 
-    private bool[] pressed = new bool[4];
-    private int[] leftValues = new int[4];
-    private int[] rightValues = new int[4];
-    private int[] finalValues = new int[4]; 
+    private int[] dispColors = new int[8];
     private int[] solution = new int[4];
-    List<int> allNums = Enumerable.Range(0, 8).ToList();
     int stage = 0;
 
 
     void Awake()
     {
         moduleId = moduleIdCounter++;
-        foreach (KMSelectable button in Buttons) 
-            button.OnInteract += delegate () { ButtonPress(Buttons.IndexOf(button)); return false; };
-        Module.OnActivate += delegate () { StartCoroutine(GridFadeIn(LeftGrid, leftValues, leftCB)); StartCoroutine(GridFadeIn(RightGrid, rightValues, rightCB)); };
+        for (int i = 0; i < 8; i++)
+        {
+            int ix = i;
+            buttons[ix].OnInteract += delegate () { ButtonPress(ix); return false; };
+        }
+        Module.OnActivate += DoFade;
     }
 
     void ButtonPress(int pos)
     {
-        if (pressed[pos])
-            return;
-        Buttons[pos].AddInteractionPunch(0.3f);
+        buttons[pos].AddInteractionPunch(0.5f);
         if (moduleSolved)
+            Audio.PlaySoundAtTransform("Chime" + ((pos % 4) + 1), transform);
+        else if (dispColors[pos] == solution[stage])
         {
-            Audio.PlaySoundAtTransform("Chime" + pos, Buttons[pos].transform);
-            return;
-        }
-        if (pos == solution[stage])
-        {
-            Audio.PlaySoundAtTransform("Chime" + stage, Buttons[pos].transform);
-            StartCoroutine(Fade(Buttons[pos].GetComponent<MeshRenderer>(), 0.5f, null));
-                        stage++;
-            Debug.LogFormat("[RGB Combination #{0}] You pressed button {1}, that is correct.", moduleId, pos + 1);
+            Debug.LogFormat("[RGB Combination #{0}] Pressed {1}.", moduleId, colorNames[dispColors[pos]]);
+            StartCoroutine(Fade(stageLEDs[stage++], Color.white, 0.5f));
+            Audio.PlaySoundAtTransform("Chime" + stage, transform);
             if (stage == 4)
             {
                 moduleSolved = true;
-                for (int i = 0; i < 4; i++)
-                    pressed[i] = false;
+                Debug.LogFormat("[RGB Combination #{0}] Module solved.", moduleId);
                 Module.HandlePass();
-                Debug.LogFormat("[RGB Combination #{0}] Module solved!", moduleId);
             }
-            if (!moduleSolved)
-                pressed[pos] = true;
         }
         else
         {
-            Debug.LogFormat("[RGB Combination #{0}] You pressed button {1}, strike.", moduleId, pos + 1);
+            Debug.LogFormat("[RGB Combination #{0}] Pressed {1} while expected {2}. Strike!", moduleId, colorNames[dispColors[pos]], colorNames[solution[stage]]);
             Module.HandleStrike();
         }
     }
-
     void Start()
     {
-        cbON = Colorblind.ColorblindModeActive;
-        SetCB();
         GenerateAnswer();
-        LogThimgy();
+        DoLogging();
     }
-
     void GenerateAnswer()
     {
+        dispColors = Enumerable.Range(0, 8).ToArray().Shuffle();
         for (int i = 0; i < 4; i++)
-        {
-            leftValues[i] = allNums.PickRandom();
-            allNums.Remove(leftValues[i]);
-            rightValues[i] = allNums.PickRandom();
-            allNums.Remove(rightValues[i]);
-            finalValues[i] = leftValues[i] ^ rightValues[i];
-        }
-        solution = Enumerable.Range(0, 4).OrderBy(x => finalValues[x]).ToArray();
+            solution[i] = dispColors[i] ^ dispColors[i + 4];
     }
-    void LogThimgy()
+    void DoLogging()
     {
-        Debug.LogFormat("[RGB Combination #{0}] The left grid's colors are {1}.", moduleId, leftValues.Select(x => Colors[x].name).Join(", "));
-        Debug.LogFormat("[RGB Combination #{0}] The right grid's colors are {1}.", moduleId, rightValues.Select(x => Colors[x].name).Join(", "));
-        Debug.LogFormat("[RGB Combination #{0}] The result grid's colors are {1}, which corresponds to the order {2}.", moduleId, finalValues.Select(x => Colors[x].name).Join(", "), solution.Select(x => x+1).Join());
+        Debug.LogFormat("[RGB Combination #{0}] The left grid has colors {1}.", moduleId, dispColors.Take(4).Select(x => colorNames[x]).Join(", "));
+        Debug.LogFormat("[RGB Combination #{0}] The right grid has colors {1}.", moduleId, dispColors.Skip(4).Select(x => colorNames[x]).Join(", "));
+        Debug.LogFormat("[RGB Combination #{0}] The solution colors are {1}.", moduleId, solution.Select(x => colorNames[x]).Join(", "));
     }
-
-    void SetCB()
+    void DoFade()
     {
-        for (int i = 0; i < 4; i++)
-        {
-            leftCB[i].gameObject.SetActive(cbON);
-            rightCB[i].gameObject.SetActive(cbON);
-        }
+        StartCoroutine(GridFadeIn(leds.Take(4).ToArray(), dispColors.Take(4).ToArray(), cbTexts.Take(4).ToArray()));
+        StartCoroutine(GridFadeIn(leds.Skip(4).ToArray(), dispColors.Skip(4).ToArray(), cbTexts.Skip(4).ToArray()));
     }
 
-    IEnumerator GridFadeIn(MeshRenderer[] renderers, int[] values, TextMesh[] texts)
+    void ToggleCB()
     {
-        yield return new WaitForSeconds(UnityEngine.Random.Range(0f, 0.5f));
-        int[] order = Enumerable.Range(0, 4).ToArray().Shuffle();
+        cbON = !cbON;
+        for (int i = 0; i < 8; i++)
+            if (cbON && dispColors[i] != 7 && dispColors[i] != 0)
+                cbTexts[i].text = colorNames[dispColors[i]][0].ToString();
+    }
+
+    IEnumerator GridFadeIn(MeshRenderer[] renderers, int[] values, TextMesh[] texts = null)
+    {
         for (int i = 0; i < 4; i++)
         {
-            int pos = order[i];
-            renderers[pos].material = Colors[values[pos]];
-            if (values[pos] != 0 && values[pos] != 7) //We don't need to enable cb support for black & white
-                texts[pos].text = Colors[values[pos]].name.Substring(0,1);
-            StartCoroutine(Fade(renderers[pos], 3, texts[pos]));
-            yield return new WaitForSeconds(0.3f);
+            StartCoroutine(Fade(renderers[i], colors[values[i]], 1.0f, texts[i]));
+            yield return new WaitForSeconds(0.75f);
         }
     }
 
-    IEnumerator Fade(MeshRenderer renderer, float time, TextMesh text)
+    IEnumerator Fade(MeshRenderer renderer, Color target, float time, TextMesh text = null)
     {
-        Debug.Log("started");
-        for (float i = 0.01f; i < time; i += Time.deltaTime)
+        renderer.material = unlit;
+        float delta = 0;
+        while (delta < 1)
         {
-            renderer.material.SetFloat("_Lerp", i / time);
-            renderer.UpdateGIMaterials();
+            delta += Time.deltaTime / time;
+            renderer.material.color = Color.Lerp(unlit.color, target, delta);
             if (text != null)
-                text.color = Color.Lerp(new Color(1, 1, 1, 0), new Color(1, 1, 1, 1), i / time);
+                text.color = Color.Lerp(Color.clear, Color.white, delta);
             yield return null;
         }
-        renderer.material.SetFloat("_Lerp", 1);
     }
 
     #pragma warning disable 414
-    private readonly string TwitchHelpMessage = @"Use <!{0} press 1234> to press those buttons in reading order. Use <!{0} colorblind> to toggle colorblind mode.";
+    private readonly string TwitchHelpMessage = @"Use <!{0} press RGBCMYWK> to press those colors. Every color is abbreviated by their first letter except black. Black is identified by 'K'. Use <!{0} colorblind> to toggle colorblind mode.";
     #pragma warning restore 414
 
     IEnumerator ProcessTwitchCommand (string command)
     {
         command = command.Trim().ToUpperInvariant();
-        List<string> parameters = command.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
-
-        if (new string[] { "COLORBLIND", "COLOURBLIND", "COLOR-BLIND", "COLOUR-BLIND", "CB", "I HATE COLORS" }.Contains(command))
+        Match m = Regex.Match(command, @"^(?:PRESS\s+)?((?:[KBGCRMYW]\s*)+)$");
+        if (command.EqualsAny("COLORBLIND", "COLOURBLIND", "COLOR-BLIND", "COLOUR-BLIND", "CB", "I HATE COLORS"))
         {
             yield return null;
-            cbON = !cbON;
-            SetCB();
-            yield break;
+            ToggleCB();
         }
-
-        if (parameters.First() != "PRESS")
-            yield break;
-        parameters.RemoveAt(0);
-        if (parameters.All(param => param.All(ch => "1234".Contains(ch))))
+        else if (m.Success)
         {
             yield return null;
-            foreach (string param in parameters)
+            foreach (int color in m.Groups[1].Value.Where(x => x != ' ').Select(x => "KBGCRMYW".IndexOf(x)))
             {
-                foreach (char ch in param)
-                {
-                    Buttons[ch - '1'].OnInteract();
-                    yield return new WaitForSeconds(0.3f);
-                }
+                buttons[dispColors.IndexOf(color)].OnInteract();
+                yield return new WaitForSeconds(0.3f);
             }
         }
+
+        
     }
 
     IEnumerator TwitchHandleForcedSolve ()
     {
-        while (!moduleSolved)
+        for (int i = stage; i < 4; i++)
         {
-            Buttons[solution[stage]].OnInteract();
+            buttons[dispColors.IndexOf(solution[i])].OnInteract();
             yield return new WaitForSeconds(0.3f);
         }
     }
